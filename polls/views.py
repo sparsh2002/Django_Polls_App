814cccb6bb7f230f326fc8a29f6cb0e1f73f6340
sparsh122002 <sparsh@futurestic.co.in>
2023-03-15 23:06:21 +0530

    css setup

--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
-
+from django.utils import timezone
 from .models import Choice, Question
 
 
@@ -27,3 +27,12 @@ class ResultsView(generic.DetailView):
 
 def vote(request, question_id):
     ... # same as above, no changes needed.
+
+def get_queryset(self):
+    """
+    Return the last five published questions (not including those set to be
+    published in the future).
+    """
+    return Question.objects.filter(
+        pub_date__lte=timezone.now()
+    ).order_by('-pub_date')[:5]
